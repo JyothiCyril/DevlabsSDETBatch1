421045f8a1b93bc1db2d4336347bcf9f992077b5
JyothiCyril <pjyothi.lakshmi@gmail.com>
2021-02-13 14:46:45 +0530

    This is First Commit

---
+++ src/Test1.java
@@ -0,0 +1,11 @@
+
+public class Test1 extends TestBase {
+
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("I am Test case 1 class");
+		
+	}
+}
---
+++ src/TestBase.java
@@ -0,0 +1,9 @@
+
+public class TestBase {
+	
+public static void main(String[] args) {
+	
+	System.out.println("I am Test base class");
+	
+}
+}
