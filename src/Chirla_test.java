82008d87076eb0714d6d048dfeaef2fada9a0a2a
chirlaa <anil.chirla@broadridge.com>
2021-02-14 11:12:13 +0530

    C

--- src/Chirla_test.java
+++ src/Chirla_test.java
@@ -4,6 +4,25 @@ public class Chirla_test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		  DesiredCapabilities    Cap = new DesiredCapabilities();
+	        
+	        Cap.setBrowserName("chrome");
+	        Cap.setPlatform(Platform.WINDOWS);
+	        
+	        ChromeOptions COptions = new ChromeOptions();
+	        COptions.merge(Cap);
+	        
+	        String HubUrl= "http://192.168.29.62:4444/wd/hub"; 
+	        
+	        WebDriver Driver = new RemoteWebDriver(new URL(HubUrl),COptions);
+	        
+	        
+	        Driver.get("https://www.amazon.in/");
+	        
+	        System.out.println(Driver.getTitle());
+	        
+	        Driver.quit();
+		
 	}
 
 }
